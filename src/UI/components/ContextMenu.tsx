a1d804ebad87e839e4f738d00146665bc74e57f2
zero <theloostt@live.com>
2022-10-10 02:45:54 -0300

    refactor: remove styled-components implementation

--- src/UI/components/ContextMenu.tsx
+++ src/UI/components/ContextMenu.tsx
@@ -1,35 +1,19 @@
-import styled from "styled-components";
-
-const ContextMenu_css = styled.ul<{xPos: number, yPos: number}>`
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    border: lightblue;
-    background-color: black;
-    border-radius: 8px;
-    position: absolute;
-    ${({xPos, yPos}) => `
-      top: ${yPos}px;
-      left: ${xPos}px;
-    `}
-
-    > li {
-      width: 200px;
-      margin: 5px;
-      padding: 0 5px;
-      border-radius: 5px;
-      color: lightblue;
-
-      :hover {
-        background-color: #272727;
-      }
-    }
-  `
-
 export function ContextMenu(props: {xPos: number, yPos: number, options: {title: string, action(): void}[]}) {
   const {xPos, yPos, options} = props;
   
-  return  <ContextMenu_css xPos={xPos} yPos={yPos}>
-            {options.map( option => <li onClick={option.action}>{option.title}</li>)}
-          </ContextMenu_css>
+  return  <div
+            className="list-none p-0 m-0 border-blue-300 rounded-lg absolute bg-gray-800"
+            style={{
+              top: yPos,
+              left: xPos
+            }}
+          >
+            {
+            options.map( option => (
+              <li 
+                className="w-52 m-1 p-0 px-1 rounded-md text-blue-300 hover:bg-gray-600 cursor-pointer select-none"
+                onClick={option.action}>{option.title}</li>
+            ))
+            }
+          </div>
 }
