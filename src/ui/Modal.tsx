63cb7238794fb6ff5b748c3bd17c2fe3702402be
z3roo <theloostt@live.com>
2023-01-04 21:39:54 -0300

    feat: stop modal event propagation

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useRef } from "react";
+
 export default function Modal(props: {children:JSX.Element|JSX.Element[], close: () => void}) {
   const { children, close } = props;
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!divRef.current)
+      return;
+    
+    const node = divRef.current as HTMLElement;
+    node.onmousedown = (e) => {
+      e.stopPropagation();
+    }
+
+    return () => {node.onmousedown = null};
+
+  },[divRef.current]);
 
   return (
-    <div className='fixed z-50 top-0 left-0 w-full h-screen flex justify-center items-center bg-gray-800 bg-opacity-60'>
+    <div ref={divRef} className='fixed z-[250] top-0 left-0 w-full h-screen flex justify-center items-center bg-gray-800 bg-opacity-60'>
       <div className='rounded p-2 relative bg-gray-300'>
         {children}
       <button 
@@ -16,9 +32,23 @@ export default function Modal(props: {children:JSX.Element|JSX.Element[], close:
 
 export function FullScreenModal(props: {children:JSX.Element|JSX.Element[], close: () => void}) {
   const { children, close } = props;
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!divRef.current)
+      return;
+    
+    const node = divRef.current as HTMLElement;
+    node.onmousedown = (e) => {
+      e.stopPropagation();
+    }
+
+    return () => {node.onmousedown = null};
+
+  },[divRef.current]);
 
   return (
-    <div className='fixed z-50 top-0 left-0 w-full h-screen flex justify-center items-center'>
+    <div ref={divRef} className='fixed z-[100] top-0 left-0 w-full h-screen flex justify-center items-center'>
       {children}
       <button 
         onClick={close}
