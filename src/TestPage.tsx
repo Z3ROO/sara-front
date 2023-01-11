e2e6abff2fb31ecef72ac04db2bf3bf3e301be37
z3roo <theloostt@live.com>
2023-01-10 21:39:19 -0300

    ...

--- src/TestPage.tsx
+++ src/TestPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, createContext, useContext, useRef } from "react"
 import { SkillTree } from "./features/Leveling/skills/components/SkillsTreeView";
 import { ISkill } from "./features/Leveling/skills/SkillsAPI";
+import { InputWithOptions } from "./ui/forms";
 
 const ThemeContext = createContext<ControllerType|null>(null);
 
@@ -30,12 +31,27 @@ function Controller() {
 }
 
 
-
+const options = [
+  {title: 'uno', value: 'suno'},
+  {title: 'dos', value: 'sdos'},
+  {title: 'tres', value: 'stres'},
+  {title: 'quatro', value: 'squatro'},
+  {title: 'cinco', value: 'scinco'},
+  {title: 'seis', value: 'sseis'},
+  {title: 'sete', value: 'ssete'},
+  {title: 'oito', value: 'soito'},
+  {title: 'nove', value: 'snove'},
+  {title: 'dez', value: 'sdez'},
+  {title: 'onze', value: 'sonze'}
+]
 
 
 export default function TestPage() {
+  const [input, setInput] = useState('');
 
   return  <div className="bg-gray-650 h-full w-screen">
+            {/* <h4>{input}</h4> */}
+            {/* <InputWithOptions<string> options={options} defaultValue={''} setValue={setInput} /> */}
             <SkillTree />
           </div>
 }
