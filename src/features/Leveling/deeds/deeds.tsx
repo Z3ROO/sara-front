c3cc01da54519a293670bbce0f63eaee8b5db296
z3roo <theloostt@live.com>
2023-01-13 12:04:33 -0300

    feat: initial deeds functionality

--- src/features/Leveling/deeds/deeds.tsx
+++ src/features/Leveling/deeds/deeds.tsx
@@ -1,6 +1,94 @@
+import { useEffect, useState } from "react";
+import { InputList, InputWithOptions, Label } from "../../../ui/forms";
+import Modal from "../../../ui/Modal";
+import { useSkillTree_SC } from "../skills/SkillsStateController";
+
+export interface IDeed {
+  _id: string
+  action_skill_id: string
+  description: string
+  todos: string[]
+  complete: boolean
+  history: {
+    date: Date
+  }[]
+}
+
+export type INewDeed = Omit<IDeed, '_id'|'history'|'complete'>
+
 export function Deeds() {
+  const { deeds } = useSkillTree_SC()!;
+
+  return (
+    <div className="w-full h-full relative">
+      <AddDeed/>
+      <div className="w-full h-full p-8 pt-20">
+        {
+        deeds.map(deed => {
+          const { description } = deed;
+
+          return (
+            <div className="w-48 h-16 bg-gray-300 border border-gray-500 rounded-sm mr-4 mb-4 inline-block">
+              {description}
+            </div>
+          )
+        })
+        }
+      </div>
+    </div>
+  )
+}
+
+function AddDeed() {
+  const { addNewDeed } = useSkillTree_SC()!;
+  const [displayModal, setDisplayModal] = useState(false);
+
+  const [action_skill_id, setAction_skill_id] = useState('');
+  const [description, setDescription] = useState<string>('');
+  const [todos, setTodos] = useState<string[]>(['']);
+
   return (
     <>
+      <button 
+        onClick={() => {
+          setDisplayModal(prev => !prev)
+        }}
+        className="absolute top-4 left-4 rounded-sm p-2 px-3 bg-gray-350">
+        <span>+ | New deed</span>
+      </button>
+      {
+        displayModal && (
+          <Modal close={() => setDisplayModal(false)}>
+            <div>
+              <h4>Add New Deed</h4>
+            </div>
+            <form id="add-deed-form">
+              <Label title="Description: ">
+                <textarea maxLength={51} value={description} onChange={e => setDescription(e.target.value)} />
+              </Label>
+              <Label title="Action Skill: ">
+                <InputWithOptions options={[]} initValue={''} value={action_skill_id} setValue={setAction_skill_id} />
+              </Label>
+              <Label title="To-do list: ">
+                <InputList value={todos} setValue={val => setTodos(val)} /> 
+              </Label>
+            </form>
+            <button
+              type="submit" form="add-deed-form"
+              onClick={e => {
+                e.preventDefault();
+                addNewDeed({
+                  description,
+                  action_skill_id,
+                  todos
+                })
+              }}
+            >
+              Create
+            </button>
+          </Modal>
+        )
+      }
     </>
   )
 }
